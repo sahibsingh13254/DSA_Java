460ace847c9fe03830af640853cd8ce5ddd64f4c
sahibsingh13254 <sahibs98765432@gmail.com>
2026-07-28 23:35:12 +0530

    Added Anagram Check

---
+++ Phase-1-Conditionals/Basic_java_code/AnagramCheck.java
@@ -0,0 +1,30 @@
+package Basic_java_code;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+        public static boolean isAnagram(String str1, String str2) {
+            str1 = str1.toLowerCase();
+            str2 = str2.toLowerCase();
+
+            if (str1.length() != str2.length()) {
+                return false;
+            }
+
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+
+            Arrays.sort(arr1);
+            System.out.println(arr1);
+            Arrays.sort(arr2);
+
+            return Arrays.equals(arr1, arr2);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isAnagram("listen", "silent")); // true
+            System.out.println(isAnagram("hello", "world"));    // false
+        }
+    }
+
